71c15fafe8839b0685adf1aff8da423f06f52b3e
Dan Brooks <dmbrooks@live.co.uk>
2018-01-09 22:16:54 +0000

    Refactor following review

--- src/InverseIndexLab.hs
+++ src/InverseIndexLab.hs
@@ -1,9 +1,13 @@
 module InverseIndexLab where
 
-import Data.List as L
-import Data.Map.Strict as M
-import Data.Set as S
-import System.Random
+import qualified Data.List       as L
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import           Data.Set (Set)
+import qualified Data.Set        as S
+import           System.Random
+
+type WordMap = Map String (Set Int)
 
 -- Task 0.6.2
 movieReview :: String -> StdGen -> String
@@ -14,33 +18,42 @@ movieReview _ seed = reviews !! select
 
 -- Task 0.6.6
 makeInverseIndex :: [String] -> Map String (Set Int)
-makeInverseIndex = L.foldr (unionWith S.union . uncurry group) M.empty . zipWithIndex . L.map words
+makeInverseIndex = M.unionsWith S.union . concat . zipWith insert [0..] . L.map words
   where
-    zipWithIndex :: [a] -> [(Int, a)]
-    zipWithIndex xs = zip [0..length xs] xs
+    insert idx = map $ flip singleton idx
 
-    group :: Ord k => v -> [k] -> Map k (Set v)
-    group n = M.fromList . L.map (\w -> (w, S.singleton n))
+singleton :: String -> Int -> WordMap
+singleton k v = M.singleton k (S.singleton v)
 
 -- Task 0.6.7
-orSearch :: [String] -> Map String (Set Int) -> Set Int
+orSearch :: (Ord k, Ord v) => [k] -> Map k (Set v) -> Set v
 orSearch ws =
   M.foldr S.union S.empty . pick ws
 
 -- Task 0.6.8
-andSearch :: [String] -> Map String (Set Int) -> Set Int
-andSearch ws index =
-  M.foldr S.intersection (S.fromList [0..length index-1]) . pick ws $ index
+andSearch :: (Ord k, Ord v) => [k] -> Map k (Set v) -> Set v
+andSearch ws index
+  | M.null result = S.empty
+  | otherwise   = foldr1 S.intersection result
+  where
+    result = pick ws index
 
 pick :: Ord k => [k] -> Map k v -> Map k v
 pick ks m =
-  restrictKeys m $ S.fromList ks
+  M.restrictKeys m $ S.fromList ks
 
-storiesBig :: IO [String]
+storiesBig :: IO (Map String (Set Int))
 storiesBig =
-  lines <$> readFile "./InverseIndexLab/stories_big.txt"
+  loadFile "./src/InverseIndexLab/stories_big.txt"
 
-storiesSmall :: IO [String]
+storiesSmall :: IO (Map String (Set Int))
 storiesSmall =
-  lines <$> readFile "./InverseIndexLab/stories_small.txt"
+  loadFile "./src/InverseIndexLab/stories_small.txt"
 
+loadFile :: String -> IO (Map String (Set Int))
+loadFile path =
+  makeInverseIndex . lines <$> readFile path
+
+exampleSearch :: String -> IO (Set Int)
+exampleSearch q =
+  andSearch (words q) <$> storiesBig
